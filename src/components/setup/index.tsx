e73c6cc9ebe36d3ffb10edc229dec90109270663
MrMahone <hannes.arthen@gmx.net>
2023-10-04 14:29:37 +0200

    feat: style player cards and install bootstrap icon library

--- src/components/setup/index.tsx
+++ src/components/setup/index.tsx
@@ -1,6 +1,7 @@
 import { PlayerCreator } from "../player-creator";
-import { Button } from "react-bootstrap";
+import { XLg } from 'react-bootstrap-icons';
 import { DiceValue } from "../../enums";
+import './setup.css';
 
 export type Player = {
     name: string;
@@ -20,10 +21,12 @@ export const Setup = (props: SetupBoxProps) => {
      * Some cards, each represents a player.
      */
     const playerCards = players.map((player) => (
-        <div className="card" key={`player-card-${player.name}`}>
-            <p key={`player-card-name${player.name}`}>{player.name}</p>
-            <p key={`player-card-dice${player.name}`}>{player.dice}</p>
-            <Button onClick={() => onRemovePlayer(player)}>X</Button>
+        <div className="player-card" key={`player-card-${player.name}`}>
+            <div className="player-data">
+                <p key={`player-card-name${player.name}`} className="player-name-text">{player.name}</p>
+                <p key={`player-card-dice${player.name}`}>Dice: {player.dice}</p>
+            </div>
+            <figure className="remove-player-button" onClick={() => onRemovePlayer(player)}><XLg width={24} height={24}/></figure>
         </div>
     ));
 
